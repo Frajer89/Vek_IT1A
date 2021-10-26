bd2d9c953934c42a0f10d67fb552f918cfddc700
Kryštof Ondřej <Krystof.Ondrej@stud.vassboskovice.cz>
2021-10-26 09:23:38 +0200

    Přidejte soubory projektu.

---
+++ Vek_IT1A/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Vek_IT1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            
+            
+            Console.SetCursorPosition(30, 15);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("╔═══════════════════════╗");
+            
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(30, 16);
+            Console.Write("║Zadej svůj rok narození║");
+            
+            
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.SetCursorPosition(30, 17);
+            Console.Write("╚═══════════════════════╝");
+
+           
+
+
+            Console.ReadLine();
+        }
+    }
+}
